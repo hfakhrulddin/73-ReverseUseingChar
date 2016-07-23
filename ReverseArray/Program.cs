5c5b4da10e76abe71822f766110ab5c1a4ca3da0
Hussen Fakhrulddin <hf556482@gmail.com>
2016-07-23 00:34:59 -0700

    init

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Wrod = Console.ReadLine();
+            Char[] newWord = ReverseArray(Wrod);
+            Console.WriteLine(newWord);
+            Console.ReadLine();
+        }
+
+        static Char[] ReverseArray(string s)
+        {
+            Char[] ReverseWrod = new Char[s.Length];
+            Char[] newS = s.ToCharArray();
+
+            for (int x=s.Length-1; x>=0 ; x--)
+            {
+                ReverseWrod[s.Length - 1 - x] = newS[x];
+            }
+
+            return (ReverseWrod);
+        }     
+    }
+}
